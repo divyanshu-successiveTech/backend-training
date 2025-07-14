da51c2f2a9b1568970242110c52a817f0df0d60a
Divyanshu <divyanshu.bhardwaj@successive.tech>
2025-07-14 10:03:10 +0530

    Assignment-2 Questions

---
+++ backend-ts/src/mockData.ts
@@ -0,0 +1,10 @@
+const mockData:{id:number,data:string}[] =[
+    {id:1,data:"Data1"},
+    {id:2,data:"Data2"},
+    {id:3,data:"Data3"},
+    {id:3,data:"Data4"},
+    {id:5,data:"Data5"},
+]
+
+export default mockData
+
